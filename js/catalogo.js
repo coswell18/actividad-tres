336455627996d8c85b56940ce98de2eea8018bdf
coswell18 <coswell18@hotmail.com>
2024-02-04 00:47:22 -0500

    Feat: Se implementa: - Consumo de servicio Api público con fetch https://randomuser.me/api/?results=6
    - Se realizan cargas dinámicas en cuadrícula de cards, sobre la página de de catálogo de doctores usando la api pública.
    
    - Se almacena en sessionStorage los doctores favoritos, haciendo click sobre cada card.
    
    - Se visualizaliza en modal el listado de los doctores favoritos consultando en sessionStorage
    
    - Se crearon controles de filtrado por campo de texto y filtros por género.
    
    - Se crean estilos, maquetación.
    
    - Se usan modales, botones, campos de texto, cards de bootstrap.
    
    - Se crea función para limpiar el sessionStorage.

---
+++ js/catalogo.js
@@ -0,0 +1,107 @@
+let users = []
+let usersFiltered = []
+let userSelected;
+document.addEventListener('DOMContentLoaded', ()=> {
+    fetch('https://randomuser.me/api/?results=6')
+    .then(response => response.json())
+    .then(data => {
+        try {
+            users = data.results;
+            setData(users);
+        } catch (error) {
+            console.log(error)
+        }  
+    });
+})
+
+const setData = (users)=>{
+    let html = ``;
+    users.map((user)=>{
+        html += `<div class="card col-12 col-md-4 col-xl-3 m-3 " >
+                    <a data-bs-toggle="modal" data-bs-target="#addUserModal" onclick="sendUserToModal('${user.email}')">
+                        <img src="${user.picture.large}" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">${user.name.first} ${user.name.last}</h5>
+                            <p class="card-text">${user.location.country}</p>
+                            <p class="card-text" ><small class="text-muted">${user.email}</small></p>
+                            <p class="card-text"><small class="text-muted">${user.cell}</small></p>
+                        </div>
+                    </a>
+                </div>`
+    }) 
+    let container = document.getElementById("container-cards")
+    container.innerHTML = html;
+} 
+
+const setDataFavorites = ()=>{
+    let html = ``;
+    let favoritesUser = JSON.parse(sessionStorage.getItem("users"))
+    if(favoritesUser != null){
+        favoritesUser.map(user=>{
+            html += `<div class="card col-12 col-md-4 col-xl-3 m-3 ">
+            
+                <img src="${user.picture.large}" class="card-img-top" alt="...">
+                <div class="card-body">
+                <h5 class="card-title">${user.name.first} ${user.name.last}</h5>
+                <p class="card-text">${user.location.country}</p>
+                <p class="card-text" ><small class="text-muted">${user.email}</small></p>
+                <p class="card-text"><small class="text-muted">${user.cell}</small></p>
+                </div>
+                </div>`
+        }) 
+        let container = document.getElementById("container-cards-favorites")
+        container.innerHTML = html;
+    } else {
+        console.log("Sin datos")
+    }
+}
+
+const fitlerUsersGender = (gender) => {
+    if(gender==="all"){
+        setData(users)
+    }else{
+        usersFiltered =  users.filter(user => user.gender === gender);
+        setData(usersFiltered)
+    }
+}
+
+const filterUsersName = (text) => {
+    if(text===""){
+        setData(users)
+    }else{
+        usersFiltered =  users.filter(user => JSON.stringify(user).toLowerCase().includes(text.value.toLowerCase()));
+        setData(usersFiltered)
+    }
+}
+
+const sendUserToModal = (email)=>{
+    let emailTmp = document.getElementById("user")
+    let user = users.find(user=> user.email === email)
+    emailTmp.innerHTML = user.name.first+" "+user.name.last
+    userSelected = user;
+}
+
+const saveUserSessionStorage = ()=>{
+    let users = sessionStorage.getItem("users");
+    if(users){
+        users = JSON.parse(users);
+        users.push(userSelected);
+    }else{
+        users =[];
+        users.push(userSelected);
+    }
+    sessionStorage.setItem('users',JSON.stringify(users))
+}
+
+const deleteUserSessionStorage = ()=>{
+    let users = sessionStorage.getItem("users");
+    if(users){
+        sessionStorage.clear("users")
+        let container = document.getElementById("container-cards-favorites")
+        container.innerHTML = "";
+        document.querySelector("#alerta").style.display = "block"
+        setTimeout(() => {
+            document.querySelector("#alerta").style.display = "none"
+        }, 2000);
+    }
+}
