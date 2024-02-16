0d76bb6c69d94de73dc0806ec00e74187f6688ed
coswell18 <coswell18@hotmail.com>
2024-02-15 20:46:33 -0500

    FIx: Ajustes finales

--- js/bienvenido.js
+++ js/bienvenido.js
@@ -26,6 +26,30 @@ const saveForm = (event)=>{
       }
   }
 
+
+const showProfile = () => {
+  if(localStorage.getItem("profile")){
+    const profile = JSON.parse(localStorage.getItem("profile"))
+    document.querySelector("#title-modal").innerHTML = `<b>Hola</b> ${profile.name} ${profile.last_name} !!!`;
+    document.querySelector("#name_m").innerHTML = `<b>Nombre</b>: ${profile.name}`;
+    document.querySelector("#last_name_m").innerHTML = `<b>Apellidos</b>: ${profile.last_name}`;
+    document.querySelector("#email_m").innerHTML = `<b>Correo</b>: ${profile.email}`;
+    document.querySelector("#age_m").innerHTML = `<b>Edad</b>: ${profile.age}`;
+    document.querySelector("#id_m").innerHTML = `<b>Cédula</b>: ${profile.id}`;
+    document.querySelector("#gender_m").innerHTML = `<b>Género</b>: ${profile.gender}`;
+    document.querySelector("#name").value = `${profile.name}`;
+    document.querySelector("#last_name").value = `${profile.last_name}`;
+    document.querySelector("#email").value = `${profile.email}`;
+    document.querySelector("#age").value = `${profile.age}`;
+    document.querySelector("#id").value = `${profile.id}`;
+    document.querySelector("#gender").value = `${profile.gender}`;
+    
+  }else{
+      document.querySelector("#subtitle").innerHTML = `RECUERDA INGRESAR LOS DATOS DE PERFIL DEPUÉS DE INICIAR SESIÓN`;
+  }
+  document.querySelector("#btn-profile").click();
+}
+
   const goPage = ()=>{
     location.href = "/catalogo.html"
   }
